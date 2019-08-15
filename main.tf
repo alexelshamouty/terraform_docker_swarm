c790ae3788433892e5a6ff20a8f56d263c24197d
Alex Elshamouty <alex.elshamouty@gmail.com>
2019-08-15 22:28:54 +0200

    Skeleton of the instances

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ terraform {
     backend "gcs" {}
 }
 
+provider "google" {
+  project     = "my-project-id"
+  region      = "us-central1"
+}
+
+resource "google_compute_network" "swarm_network" {
+    name = var.gcp_network_name
+    description = "Docker swarm network for ${var.gcp_environment}"
+    auto_create_subnetworks = true
+}
+
+resource "google_compute_instance" "worker" {
+    count = var.gcp_workers_number
+}
---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "gcp_project_id" {}
+variable "gcp_region" {}
+variable "gcp_image_id" {}
+variable "gcp_worker_instance_type" {}
+variable "gcp_manager_instance_type"{}
+variable "gcp_workers_number" {}
+variable "gcp_nodes_number" {}
+variable "gcp_ssh_key_path" {}
+variable "gcp_user_name" {}
+variable "gcp_network_name" {}
+variable "gcp_zone" {}
+variable "gcp_environment" {}
