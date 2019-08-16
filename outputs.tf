74b907e3e8f60c4b8982ac3676502aa1de7b7cad
Alex Elshamouty <alex.elshamouty@gmail.com>
2019-08-16 16:22:28 +0200

    Typo fix in the manager/worker resources from the root module Fixed in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,15 +2,15 @@ data "google_compute_instance" "nat_ips" {
     name = "*"
     zone = var.gcp_zone
     depends_on = [
-        "google_compute_instance.workers",
-        "google_compute_instance.managers"
+        "google_compute_instance.worker",
+        "google_compute_instance.manager"
     ]
 }
 
 output "worker_ips" {
-    value = "google_compute_instance.workers.*.network_interface.0.access_config.0.nat_ip"
+    value = "google_compute_instance.worker.*.network_interface.0.access_config.0.nat_ip"
 }
 
 output "managers_ips" {
-    value = "google_compute_instance.managers.*.network_interface.0.access_config.0.nat_ip"
+    value = "google_compute_instance.manager.*.network_interface.0.access_config.0.nat_ip"
 }
